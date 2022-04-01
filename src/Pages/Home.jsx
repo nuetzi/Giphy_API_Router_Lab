76c821459dd86df085f8970001e15c4fd67f59bd
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-03-31 21:18:02 -0400

    Minor CSS adjustment to random display

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -14,7 +14,7 @@ export default function Home () {
 
     return (
         <div>
-            <img src={randomGif} /> <br/>
+            <img src={randomGif} className="displayGif"/> <br/>
             <br/>
             <button 
                 onClick = {() => {
